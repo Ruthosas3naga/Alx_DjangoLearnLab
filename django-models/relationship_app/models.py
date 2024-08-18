30dd715a4433ed8f534daa78bcf2425c60642abc
osasenaga <edionwe.osasenaga5@gmail.com>
2024-08-18 22:00:23 +0100

    templates for relationship app

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -24,7 +24,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     # Define role choices
-    ROLE_CHOICES = [
+    role_choices = [
         ('Admin', 'Admin'),
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
@@ -32,7 +32,7 @@ class UserProfile(models.Model):
     
     # Link to the User model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
+    role = models.CharField(max_length=10, choices=role_choices, default='Member')
     
     def __str__(self):
         return f'{self.user.username} - {self.role}'
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -47,35 +47,25 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-from django.shortcuts import render
+# In relationship_app/views.py
+
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from .models import UserProfile
 
-# Check if the user is an Admin
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    return UserProfile.objects.filter(user=user, role='Admin').exists()
 
-# Check if the user is a Librarian
 def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == 'Librarian'
+    return UserProfile.objects.filter(user=user, role='Librarian').exists()
 
-# Check if the user is a Member
 def is_member(user):
-    return user.is_authenticated and user.userprofile.role == 'Member'
+    return UserProfile.objects.filter(user=user, role='Member').exists()
 
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'admin_view.html')
 
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'librarian_view.html')
 
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'member_view.html')
 
 
     
