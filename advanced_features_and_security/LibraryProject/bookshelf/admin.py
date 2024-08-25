198ebb3229ed8d289e53070bbc153d8ce78d3d2c
osasenaga <edionwe.osasenaga5@gmail.com>
2024-08-25 18:23:54 +0100

    settings and admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -10,3 +10,47 @@ class BookAdmin(admin.ModelAdmin):
 
 
 # Register your models here.
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+
+from .models import CustomUser
+
+# Custom forms for creating and changing users in the admin
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'date_of_birth', 'profile_photo')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ('username', 'email', 'date_of_birth', 'profile_photo')
+
+# Custom UserAdmin class
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+# Register the custom user admin
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.conf import settings
 from django.db import models
 
@@ -18,4 +18,45 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-  
+
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email, date of birth, and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not date_of_birth:
+            raise ValueError('The Date of Birth field must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email, date of birth, and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, date_of_birth, password, **extra_fields)
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    # Specify that the CustomUserManager should be used for managing CustomUser instances
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
