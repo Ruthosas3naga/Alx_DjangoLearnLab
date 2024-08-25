ea6440be18acb2462f3ef3c69f3cd8a4d0161816
osasenaga <edionwe.osasenaga5@gmail.com>
2024-08-25 21:32:51 +0100

    security settings

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -40,3 +40,19 @@ from .models import Book
 def book_list(request):
     books = Book.objects.all()  # Fetch all book records from the database
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+
+# LibraryProject/bookshelf/views.py
+
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'bookshelf/register.html', {'form': form})
