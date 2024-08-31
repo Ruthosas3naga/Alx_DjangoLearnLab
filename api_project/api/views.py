77fc8df37cacaf3c02cad85aa6c28be350aa697e
osasenaga <edionwe.osasenaga5@gmail.com>
2024-08-31 21:17:54 +0100

    API endpoint with django restframework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        book_filter = self.request.query_params.get('book', None)
+        if book_filter is not None:
+            queryset = queryset.filter(name__icontains=book_filter)
+        return queryset
