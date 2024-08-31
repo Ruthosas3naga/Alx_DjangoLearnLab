3e50972847e94ff48df88ac1a8fd6517cbfbc013
osasenaga <edionwe.osasenaga5@gmail.com>
2024-08-31 21:20:29 +0100

    API endpoint with django restframework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
