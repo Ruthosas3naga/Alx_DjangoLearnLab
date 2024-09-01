1439c363495b86599d3a378530e7d8b362221eef
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-01 20:37:09 +0100

    Routers and CRUD creation

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from .views import BookListCreateAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookListAPIView
+
+router = DefaultRouter()
+router.register(r'my-books', BookListAPIView)
 
 urlpatterns = [
-    path("api/books", BookListCreateAPIView.as_view(), name="book_list_create"),
+    path("api/books", BookListAPIView.as_view(), name="book_list_create"),
 ]
 
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from rest_framework import generics
-from rest_framework.generics import ListAPIView
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets, status
+from rest_framework.response import Response
 from .models import Book
 from .serializers import BookSerializer
 
@@ -14,3 +14,43 @@ class BookListAPIView(generics.ListAPIView):
         if book_filter is not None:
             queryset = queryset.filter(name__icontains=book_filter)
         return queryset
+
+class BookViewSet(viewsets.ViewSet):
+    
+    # Retrieve a single book by ID
+    def retrieve(self, request, pk=None):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
+
+    # Create a new book
+    def create(self, request):
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Update an existing book by ID
+    def update(self, request, pk=None):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Partially update an existing book by ID
+    def partial_update(self, request, pk=None):
+        book = get_object_or_404(Book, pk=pk)
+        serializer = BookSerializer(book, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # Delete an existing book by ID
+    def destroy(self, request, pk=None):
+        book = get_object_or_404(Book, pk=pk)
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
