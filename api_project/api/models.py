f98639347f80e7afecbc04594e6a49f5f4dcae14
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-08 23:23:20 +0100

    testing

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
     
 
 class Book(models.Model):
@@ -10,3 +13,5 @@ class Book(models.Model):
     publication_year = models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
