33a53fcb983cb2ead5428ade403112706701bb7f
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-08 22:37:28 +0100

    ordering,searching and filtering

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from .models import Book
+from .models import Book, Author
+from datetime import datetime
 
 class BookSerializer(serializers.ModelSerializer):
     class meta:
         model = Book
         fields = '__all__'
+    
+    def validate_publication_year(self, value):
+        current_year = datetime.now().year
+        if value > current_year:
+            raise serializers.ValidationError('Publication year must not be in the future.')
+        return value
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+    class meta:
+        model = Author
+        fields = ['name', 'books']
