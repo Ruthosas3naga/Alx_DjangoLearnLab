2607a51e90eb736d769c09a6f865d42021a7058a
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-08 19:30:55 +0100

    ordering,searching and filtering

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -11,6 +11,14 @@ from django_filters.rest_framework import DjangoFilterBackend
 
 
 
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'author__name', 'publication_year']
+
+
+
 class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
