e472209f57787812209e1493de55d624a8dd04e0
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-08 22:16:13 +0100

    ordering,searching and filtering

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, BookList
+from .views import ListView, CreateView, DeleteView, DetailView, UpdateView
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet, basename='book')
+router.register(r'books', ListView, basename='book')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('books/', BookList.as_view(), name='book-list'),
+    path('books/', ListView.as_view(), name='book-list'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name='book-delete'),
+    path('books/update/<int:pk>/', UpdateView.as_view(), name='book-update'),
+    path('books/detail/<int:pk>/', DetailView.as_view(), name='book-detail'),
+    
 ]
 
