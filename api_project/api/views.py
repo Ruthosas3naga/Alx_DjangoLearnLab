039014fa39a42fb5969946b86eaf2a62ef48c7e1
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-08 22:00:33 +0100

    ordering,searching and filtering

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,85 +1,23 @@
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, viewsets, status, filters
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from rest_framework.pagination import PageNumberPagination
-from django_filters import rest_framework
 
-
-class BookListAPIView(generics.ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    def get_queryset(self):
-        queryset = self.queryset
-        book_filter = self.request.query_params.get('book', None)
-        if book_filter is not None:
-            queryset = queryset.filter(name__icontains=book_filter)
-        return queryset
-
-
-class BookViewSet(viewsets.ModelViewSet):
+class BookDetail(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['title', 'author__name', 'publication_year']
-    search_fields = ['title', 'author__name']
-    ordering_fields = ['title', 'publication_year']
-    ordering = ['title']
-    pagination_class = PageNumberPagination
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-    def retrieve(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
-
-    def create(self, request):
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def update(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(book, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def partial_update(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        serializer = BookSerializer(book, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def destroy(self, request, pk=None):
-        book = get_object_or_404(Book, pk=pk)
-        book.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class AdminBookViewSet(viewsets.ModelViewSet):
+class BookCreate(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminUser]
 
-
-class BookListView(generics.ListAPIView):
+class BookUpdate(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-    filterset_fields = ['title', 'author__name', 'publication_year']
-    search_fields = ['title', 'author__name']
-    ordering_fields = ['title', 'publication_year']
-    ordering = ['title']
-    pagination_class = PageNumberPagination
+
+class BookDelete(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
