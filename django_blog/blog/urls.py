7f8de58aa08e436cb727c028aad98048ccd8ec87
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-15 13:35:05 +0100

    customization and authentication

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('register/', auth_views.register, name='register'),
-    path('profile/', auth_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.auth_view(), name='login'),
+    path('logout/', auth_views.LogoutView.auth_view(), name='logout'),
+    path('register/', auth_views.register.as_view(), name='register'),
+    path('profile/', auth_views.profile.as_view(), name='profile'),
 ]
