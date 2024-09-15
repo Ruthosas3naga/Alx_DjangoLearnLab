a19a03e645c4e3ed826b523e6d65bb4d54a0b242
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-15 19:17:16 +0100

    Comment model and crud operations

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
@@ -9,3 +10,11 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['email', 'username', 'password']
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        self.fields['content'].widget.attrs.update({'placeholder': 'Enter your comment here'})
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from blog.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import add_comment, edit_comment, delete_comment
 
 urlpatterns = [
     path('login/', auth_views.LoginView.auth_view(), name='login'),
@@ -11,5 +12,8 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),,
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:post_id>/comment/', add_comment, name='add-comment'),
+    path('comment/<int:comment_id>/edit/', edit_comment, name='edit-comment'),
+    path('comment/<int:comment_id>/delete/', delete_comment, name='delete-comment')
 ]
