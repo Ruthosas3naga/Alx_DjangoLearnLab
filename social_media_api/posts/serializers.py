81bd90d3a455a259dd4ff2ba1b125e0cbf05a755
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-20 23:10:06 +0100

    created new app called post

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,6 +1,18 @@
 from models import Post, Comment
 from rest_framework import serializers
-from django.contrib.auth import 
 
 class PostSerilizer(serializers.ModelSerializer):
-    
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Post instances.
+    """
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Assign the author as the logged-in user
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing Comment instances.
+    """
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        # Assign the author as the logged-in user
+        serializer.save(author=self.request.user)
+
+    
+
+
 
 # Create your views here.
