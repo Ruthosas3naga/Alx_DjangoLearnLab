aef4fda0be69f4c37d07c1d1bc01000b443f64f9
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-20 23:12:32 +0100

    created new app called post

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -7,8 +7,8 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=100)
     content = models.TextField()
-    created_at = models.DateField
-    updated_at = models.DateField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
 
     def __str__(self):
         return self.title
@@ -18,8 +18,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
     content = models.TextField()
-    created_at = models.DateField
-    updated_at = models.DateField
+    created_at = models.DateTimeField
+    updated_at = models.DateTimeField
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
