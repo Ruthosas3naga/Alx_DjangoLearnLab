8a36fa6e8997395ee04979d991fe93e5dd1c13c4
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-22 17:36:36 +0100

    created feed view, unfollow and follow view

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
-from django.contrib.auth import authenticate
-from .serializers import RegisterSerializer
-from .models import CustomUser
-from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate, get_user_model
+from .serializers import RegisterSerializer
+
+CustomUser = get_user_model()
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
@@ -21,17 +20,14 @@ class LoginView(generics.GenericAPIView):
         user = authenticate(username=username, password=password)
         if user:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
-        return Response({'error': 'Invalid credentials'}, status=400)
-
-
-CustomUser = get_user_model()
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 class FollowUserView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        user_to_follow = CustomUser.objects.get(pk=pk)
+        user_to_follow = get_object_or_404(CustomUser, pk=pk)
         if request.user != user_to_follow:
             request.user.following.add(user_to_follow)
             return Response({'message': 'User followed successfully.'}, status=status.HTTP_200_OK)
@@ -41,7 +37,7 @@ class UnfollowUserView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        user_to_unfollow = CustomUser.objects.get(pk=pk)
+        user_to_unfollow = get_object_or_404(CustomUser, pk=pk)
         if request.user != user_to_unfollow:
             request.user.following.remove(user_to_unfollow)
             return Response({'message': 'User unfollowed successfully.'}, status=status.HTTP_200_OK)
