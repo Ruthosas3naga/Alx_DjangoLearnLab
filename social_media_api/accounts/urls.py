d64ecde5ddd8f5452e2508db1e6f5422c957b1ae
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-22 18:04:52 +0100

    created feed view, unfollow and follow view

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,7 +4,8 @@ from .views import RegisterView, LoginView, FollowUserView, UnfollowUserView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('users/<int:pk>/follow/', FollowUserView.as_view(), name='follow-user'),
-    path('users/<int:pk>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
     # Profile management routes can be added here
 ]
+
