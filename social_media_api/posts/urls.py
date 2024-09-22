63d88378160ba13534302c4b2c36d19451ca5d9d
osasenaga <edionwe.osasenaga5@gmail.com>
2024-09-22 18:07:02 +0100

    created feed view, unfollow and follow view

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,12 +2,13 @@ from rest_framework.routers import DefaultRouter
 from django.urls import path, include
 from .views import PostViewSet, CommentViewSet, FeedView
 
+# Register viewsets with the router
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
 router.register(r'comments', CommentViewSet, basename='comment')
-router.register(r'feed/', FeedView, basename='feed')
 
+# Define URL patterns
 urlpatterns = [
     path('', include(router.urls)),
+    path('feed/', FeedView.as_view(), name='feed'),  # Use path for generic views
 ]
-
